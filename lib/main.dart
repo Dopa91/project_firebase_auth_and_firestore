96ecfed2cafc92c23288d47b53ea0c64b72b2e27
Dopa91 <code.dopatka@gmail.com>
2024-12-11 16:00:17 +0100

    LoginSCreen mit Firebase Auth eingabut- tasksheet fertig

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tasksheet_firebase_authentication/firebase_options.dart';
 
@@ -10,21 +12,83 @@ void main() async {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+final authInstance = FirebaseAuth.instance;
+
+Stream<User?> get onAuthStateChanged => authInstance.authStateChanges();
+
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  late TextEditingController mailController;
+  late TextEditingController passwordController;
+
+  Future<void> loginUser(String email, String password) async {
+    try {
+      final userCredential = await authInstance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      print("Login erfolgreich: ${userCredential.user?.email}");
+    } on FirebaseAuthException catch (e) {
+      print("Firebase Auth Exception: ${e.message}");
+    } catch (e) {
+      print("Allgemeiner Fehler: $e");
+    }
+  }
+
+  Future<void> logoutUser() async {
+    await authInstance.signOut();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    mailController = TextEditingController();
+    passwordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    mailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-            child: Column(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const TextField(),
-            FloatingActionButton(onPressed: () {}),
-            FloatingActionButton(onPressed: () {}),
+            TextField(
+              controller: mailController,
+              decoration: const InputDecoration(labelText: "E-Mail"),
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: const InputDecoration(labelText: "Password"),
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                String mail = mailController.text;
+                String password = passwordController.text;
+
+                loginUser(mail, password);
+              },
+              child: const Text("Login"),
+            ),
+            FloatingActionButton(
+              onPressed: logoutUser,
+              child: const Text("Logout"),
+            ),
           ],
-        )),
+        ),
       ),
     );
   }
