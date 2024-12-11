677a94035759e4ce79840534ac4e7c1cffc24103
Dopa91 <code.dopatka@gmail.com>
2024-12-11 15:31:38 +0100

    added Firebase_core dependencie

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:tasksheet_firebase_authentication/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MainApp());
 }
 
