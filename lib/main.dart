6548ea670c7ae2d327644af393e3578cf3a36844
Dopa91 <code.dopatka@gmail.com>
2024-12-12 21:36:55 +0100

    LoginLogic in own file

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:tasksheet_firebase_authentication/firebase_options.dart';
-import 'package:tasksheet_firebase_authentication/feature/Home/home_screen.dart';
-import 'package:tasksheet_firebase_authentication/feature/Login/login_screen.dart';
+import 'package:tasksheet_firebase_authentication/main_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,31 +9,4 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MainApp());
-}
-
-final authInstance = FirebaseAuth.instance;
-final firestoreInstance = FirebaseFirestore.instance;
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: StreamBuilder<User?>(
-        stream: authInstance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-
-          if (snapshot.hasData) {
-            return const HomeScreen();
-          } else {
-            return const LoginScreen();
-          }
-        },
-      ),
-    );
-  }
 }
---
+++ lib/main_app.dart
@@ -0,0 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:tasksheet_firebase_authentication/feature/Home/home_screen.dart';
+import 'package:tasksheet_firebase_authentication/feature/Login/login_screen.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: LoginLogic(),
+    );
+  }
+}
+
+final authInstance = FirebaseAuth.instance;
+final firestoreInstance = FirebaseFirestore.instance;
+
+class LoginLogic extends StatelessWidget {
+  const LoginLogic({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: authInstance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if (snapshot.hasData) {
+          return const HomeScreen();
+        } else {
+          return const LoginScreen();
+        }
+      },
+    );
+  }
+}
